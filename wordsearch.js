3f5acc665be5801497dc75ffc8011b7de723b48d
Olalekan Isaac Ogunremi <OOgunremi@yahoo.com>
2022-03-16 16:04:23 -0600

    updated code to detect vertical words

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,32 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+// This gist was developed using Paired Programing between
+// Olalekan Isaac Ogunremi and Matthew Seligman
 
-module.exports = wordSearch
+const wordSearch = (letters, word) => {
+  const transpose = matrix => {
+    let answer = [];
+    
+    for (let rows = 0; rows < matrix.length; rows++) {
+          
+      for (let columns = 0; columns < matrix[rows].length; columns++) {
+        answer.push([]);
+        answer[columns].push(matrix[rows][columns]);
+      }
+    } return answer;
+  };
+
+    
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  const transposed = transpose(horizontalJoin).map((innerArray) => innerArray.join(""));
+
+  for (let l of transposed) {
+    if (l.includes(word)) return true;
+  }
+
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+    
+  return false;
+};
+
+module.exports = wordSearch;
